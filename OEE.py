317f122a6a86b4d711081cf914d9452564985c6d
levi <menyhartlevi@gmail.com>
2022-05-11 21:29:10 +0200

    todos partly done

--- OEE.py
+++ OEE.py
@@ -33,7 +33,7 @@ def stringToDateTime(string):
     return datetime.strptime(string, DATETIME_FORMAT)
 
 def calculateOEE(day, workstationId, jobId, _time_override=False):
-    # todo: implement _time_override behaviour for testing purposes
+    # todo: implement _time_override behaviour for testing purposes :)
     # _time_override will never be used in production,we will work with the current time
     now = datetime.now()
     if _time_override:
@@ -42,12 +42,16 @@ def calculateOEE(day, workstationId, jobId, _time_override=False):
     #integer unix time in milliseconds at 0:00:00
     timeTodayStart = int(day.timestamp()*1000)
     #integer unix time in milliseconds at 24:00:00
-    timeTodayEnd = int((day + pd.DateOffset(days=1)).timestamp()*1000)
+    if _time_override:
+        timeTodayEnd = now.timestamp()*1000
+    else:
+        timeTodayEnd = int((day + pd.DateOffset(days=1)).timestamp()*1000)
+    print(timeTodayEnd)
     
     # availability        
-    # todo: use workstationId
-    # 'urn:ngsi_ld:Workstation:1'.replace(':','_')
-    df = pd.read_sql_query(f'select * from default_service.urn_ngsi_ld_workstation_1_workstation',con=con)
+    # todo: use workstationId :)
+    workstation = workstationId.replace(':', '_').replace('W', 'w') + '_workstation'
+    df = pd.read_sql_query(f'select * from default_service.' + workstation,con=con)
     # convert timestamps to integers
     df['recvtimets'] = df['recvtimets'].map(float)
     df['recvtimets'] = df['recvtimets'].map(int)
@@ -72,21 +76,25 @@ def calculateOEE(day, workstationId, jobId, _time_override=False):
     
     # OperatorScheduleStartsAt and the OperatorScheduleStopsAt will be requested from the Orion broker
     # if the shift has not ended yet, the current time needs to be used instead of OperatorScheduleStopsAt
-    # todo: use Orion.getObject to fetch OperatorSchedule object as JSON, extract these variables
+    # todo: use Orion.getObject to fetch OperatorSchedule object as JSON, extract these variables :(
+    
+    JSON = Orion.getObject('urn:ngsi_ld:OperatorSchedule:1')
+    #print(JSON)
     OperatorScheduleStartsAt = stringToDateTime('2022-04-10 8:00:00.000')
     OperatorScheduleStopsAt = stringToDateTime('2022-04-10 16:00:00.000')
     
-    # todo return None if now is past the OperatorScheduleStopsAt value
+    # todo return None if now is past the OperatorScheduleStopsAt value :(
     
-    # todo: we do not change OperatorScheduleStopsAt, use a variable called "now" instead
+    # todo: we do not change OperatorScheduleStopsAt, use a variable called "now" instead :)
     if (df.iloc[-1].attrvalue =='true'):
-        OperatorScheduleStopsAt = datetime.now()
-    
-    availability = total_available_time/(OperatorScheduleStopsAt.timestamp()*1000-OperatorScheduleStartsAt.timestamp()*1000)
+        availability = total_available_time/(now.timestamp()*1000-OperatorScheduleStartsAt.timestamp()*1000)
+    else:
+        availability = total_available_time/(OperatorScheduleStopsAt.timestamp()*1000-OperatorScheduleStartsAt.timestamp()*1000)
     
     # performance, quality
-    # todo use jobId
-    df = pd.read_sql_query(f'select * from default_service.urn_ngsi_ld_job_202200045_job',con=con)
+    # todo use jobId :)
+    job = jobId.replace(':', '_').replace('J', 'j') + '_job'
+    df = pd.read_sql_query(f'select * from default_service.' + job,con=con)
     # convert timestamps to integers
     df['recvtimets'] = df['recvtimets'].map(float)
     df['recvtimets'] = df['recvtimets'].map(int)
@@ -94,27 +102,18 @@ def calculateOEE(day, workstationId, jobId, _time_override=False):
     df = df[(timeTodayStart < df.recvtimets) & (df.recvtimets <= timeTodayEnd)]
     
     if df.size == 0:
-        return None
+        #return None !!!!!!!!!!
+        quality = 0
+        performance = 0
     else:
-        GoodPartCounter = int(df[df.attrname == 'GoodPartCounter'].iloc[-1].attrvalue)
-        RejectPartCounter = int(df[df.attrname == 'RejectPartCounter'].iloc[-1].attrvalue)
-        TotalProductCounter = GoodPartCounter + RejectPartCounter
-    
-        quality = (TotalProductCounter - RejectPartCounter) / TotalProductCounter
+        n_successful_mouldings = len(df[df.attrname == 'GoodPartCounter']['attrvalue'].unique())
+        n_failed_mouldings = len(df[df.attrname == 'RejectPartCounter']['attrvalue'].unique())
+        n_total_mouldings = n_successful_mouldings + n_failed_mouldings
         
-        StartGoodPartCounter = int(df[df.attrname == 'GoodPartCounter'].iloc[0].attrvalue)
-        CurrentGoodPartCounter = int(df[df.attrname == 'GoodPartCounter'].iloc[-1].attrvalue)
-        TodayGoodPartCounter = CurrentGoodPartCounter - StartGoodPartCounter
+        referenceInjectionTime = 50000
+        performance = n_total_mouldings * referenceInjectionTime / total_available_time
+        quality = n_successful_mouldings / n_total_mouldings
         
-        # we already calculated the total_available_time for today, we can use it here
-        StartTime = int(df[df.attrname == 'GoodPartCounter'].iloc[0].recvtimets)
-        CurrentTime = int(df[df.attrname == 'GoodPartCounter'].iloc[-1].recvtimets)
-        FullTime = CurrentTime - StartTime
-        
-        ReferenceJobTime = 6000
-        
-        performance = ReferenceJobTime * TodayGoodPartCounter / FullTime
- 
         
     
     """
@@ -127,7 +126,7 @@ def calculateOEE(day, workstationId, jobId, _time_override=False):
     n_failed_mouldings = len(df[df['attrname'] == 'RejectPartCounter']['RejectPartCounter'].unique())
     n_total_mouldings = n_successful_mouldings + n_failed_mouldings
 
-    # todo get Job from Orion based on jobId
+    # todo get Job from Orion based on jobId :(
     # todo extract JobTpye from JSON
     # todo get urn:ngsi_ld:Constants:1 from Orion, extract referenceInjectionTime
     # referenceInjectionTime depends on the JobId - look up
@@ -138,29 +137,41 @@ def calculateOEE(day, workstationId, jobId, _time_override=False):
     quality = n_successful_mouldings / n_total_mouldings
 
     """
-    
     oee = availability * performance * quality
     
     return availability, performance, quality, oee
 
 def insertOEE(workstationId, availability, performance, quality, oee):
-    # todo insert a new row into the OEE table
+    # todo insert a new row into the OEE table :) 
     # create table if not exists, append row if it does
-    # df: DataFrame with only one row
-    # df.to_sql(name=table, con=con, schema=postgresSchema, index=False, dtype=col_dtypes, if_exists='append')
+    table_name = workstationId.replace(':', '_').replace('W', 'w') + '_workstation_oee_tryyyyyy'
+    current_time = datetime.now().timestamp()*1000
+
+    #csak akkor fogadta el a timestamp számokat, ha az oszlopok nevét átírtam valami értelmetlenre az eredetiről, mert így már nem vár idő formátumot    
+    df = pd.DataFrame(data=[[current_time,current_time - 7200000, availability, performance, quality, oee,4,'job']], columns=['xrecvtimets','xrecvtime','availability','performance','quality','oee','throughput_shift','jobs'])
+
+    df.iloc[[0]].to_sql(name=table_name, con=con, schema=postgresSchema, index=False, dtype=col_dtypes, if_exists='append')
+    
+    df_read = pd.read_sql_query(f'select * from default_service.' + table_name,con=con)
+    print(df_read)
+    
+    
     pass
 
 def testcalculateOEE():
-    day = datetime(2022, 4, 8)
+    day = datetime(2022, 4, 4)
     global engine, con
     engine = create_engine('postgresql://{}:{}@localhost:5432'.format(conf['postgresUser'], conf['postgresPassword']))
     con = engine.connect()
     # some work needs to be done on Andor's side
     #####availability, performance, quality, oee = calculateOEE(day, 'urn:ngsi_ld:Job:202200045', 'urn:ngsi_ld:Workstation:1')
     #print(f'availability: {availability}, performance: {performance}, quality: {quality}, oee: {oee}\n')
-    # todo why does it return only 2 values? I see 4.
-    [availability, quality] = calculateOEE(day, 'urn:ngsi_ld:Job:202200045', 'urn:ngsi_ld:Workstation:1')
-    print('Availability: ', availability, '\nQuality: ', quality)
+    # todo why does it return only 2 values? I see 4. :)
+    [availability, performance, quality, oee] = calculateOEE(day, 'urn:ngsi_ld:Workstation:1', 'urn:ngsi_ld:Job:202200045', stringToDateTime('2022-04-04 16:38:27.87'))
+    print('Availability: ', availability, '\nPerformance: ', performance, '\nQuality: ', quality, '\nOEE: ', oee)
+    
+    insertOEE('urn:ngsi_ld:Workstation:1', availability, performance, quality, oee)
+    
     con.close()
     engine.dispose()
 
