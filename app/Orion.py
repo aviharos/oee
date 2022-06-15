fa85a6f1514479b779f253fbf54b70665306de5e
panda-mechanic <m2vihand@gmail.com>
2022-06-15 17:39:01 +0200

    getActiveJobId, getWorkstationIds, getRequestToOrion

--- app/Orion.py
+++ app/Orion.py
@@ -26,16 +26,12 @@ if conf['log_to_stdout']:
     logger_Orion.addHandler(stream_handler_Orion)
 
 
-def getObject(object_id, host=conf['orion_host'], port=conf['orion_port']):
-    '''
-    Returns the object in JSON format idenfitied by object_id and the status code of the request
-    '''
-    url = f'http://{host}:{port}/v2/entities/{object_id}'
+def getRequestToOrion(url):
     try:
         response = requests.get(url)
         response.close()
     except:
-        logger_Orion.error(f'Get request failed to URL: {url} for unknown reason')
+        logger_Orion.error(f'Get request failed to URL: {url} ')
         return None, None
     else:
         if response.status_code == 200:
@@ -44,19 +40,52 @@ def getObject(object_id, host=conf['orion_host'], port=conf['orion_port']):
             logger_Orion.error(f'Get request failed to URL: {url}, status code:{response.status_code}')
             return response.status_code, None
 
+
+def getObject(object_id, host=conf['orion_host'], port=conf['orion_port']):
+    '''
+    Returns the object in JSON format idenfitied by object_id and the status code of the request
+    '''
+    url = f'http://{host}:{port}/v2/entities/{object_id}'
+    return getRequestToOrion(url)
+
+
 def getWorkstationIds():
-    #return workstationIds
-    pass
+    url = f'http://{conf["orion_host"]}:{conf["orion_port"]}/v2/entities?type=Workstation'
+    status_code, entities = getRequestToOrion(url)
+    workstation_ids = []
+    if status_code != 200:
+        return status_code, workstation_ids
+    for ws in entities:
+        workstation_ids.append(ws['id'])
+    return status_code, workstation_ids
+
 
 def getActiveJobId(workstationId):
-    # return jobId
-    pass
+    status_code, workstation = getObject(workstationId)
+    if status_code != 200:
+        return status_code, workstation
+    return status_code, workstation['RefJob']['value']
 
-def testgetObject():
+
+def test_getObject():
     status_code, parsedJob = getObject('urn:ngsi_ld:Job:202200045')
-    print(status_code)
-    print(parsedJob['id'])
-    print(parsedJob['GoodPartCounter']['value'])
+    logger_Orion.debug(f'status_code: {status_code}')
+    logger_Orion.debug(f'parsed jobId: {parsedJob["id"]}')
+    logger_Orion.debug(f'parsed job\'s GoodPartCounter: {parsedJob["GoodPartCounter"]["value"]}')
+
+
+def test_getworkstationIds():
+    status_code, workstation_ids = getWorkstationIds()
+    logger_Orion.debug(f'Workstation ids: {workstation_ids}')
+
+
+def test_getActiveJobId():
+    status_code1, workstation_ids = getWorkstationIds()
+    for wsid in workstation_ids:
+        status_code2, jobId = getActiveJobId(wsid)
+        logger_Orion.debug(f'Workstation id: {wsid}, Job id: {jobId}')
 
 if __name__ == '__main__':
-    testgetObject()
+    # test_getObject()
+    test_getworkstationIds()
+    test_getActiveJobId()
