8a81330eece1516cccbe737bac748d80796db0dc
aviharos <vihand@protonmail.ch>
2022-07-28 22:17:50 +0200

    added 3 examples for a Job, a script for uploading the jsons to Orion before testing with PLCs

---
+++ test/upload_jsons_to_Orion.py
@@ -0,0 +1,23 @@
+'''
+This file is meant to upload the json objects to the
+Orion broker before testing with a physical or virtual PLC.
+'''
+
+import sys
+import glob
+import json
+
+# PyPI imports
+
+# Custom imports
+sys.path.insert(0, '../app')
+import Orion
+from conf import conf
+
+if __name__ == '__main__':
+    url = f'http://{conf["orion_host"]}:{conf["orion_port"]}/v2/entities'
+    jsons = glob.glob('../json/*.json')
+    for json_ in jsons:
+        obj = json.loads(json_)
+        Orion.postObjectToOrion(url, obj)
+
