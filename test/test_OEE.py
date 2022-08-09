da629fa145ace56947e79dccb33270438609029c
aviharos <vihand@protonmail.ch>
2022-08-10 00:50:52 +0200

    test_OEE

---
+++ test/test_OEE.py
@@ -0,0 +1,29 @@
+import unittest
+import sys
+sys.path.insert(0, '../app')
+
+from OEE import OEE
+from conf import conf
+
+class testOrion(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        pass
+
+    @classmethod
+    def tearDownClass(cls):
+        pass
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_msToDateTimeString(self):
+        pass
+
+    def test_stringToDateTime(self):
+        pass
+
+    def test_
