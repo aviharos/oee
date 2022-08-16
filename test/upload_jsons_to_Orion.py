399467ee989d06c29648451f8c05d945edfcb829
aviharos <vihand@protonmail.ch>
2022-08-16 20:04:50 +0200

    fix: load jsons

--- test/upload_jsons_to_Orion.py
+++ test/upload_jsons_to_Orion.py
@@ -18,7 +18,9 @@ def main():
     url = f'http://{conf["orion_host"]}:{conf["orion_port"]}/v2/entities'
     jsons = glob.glob('../json/*.json')
     for json_ in jsons:
-        obj = json.loads(json_)
+        print(json_)
+        with open(json_, 'r') as f:
+            obj = json.load(f)
         Orion.postObjectToOrion(url, obj)
 
 if __name__ == '__main__':
