f76824594cc2a16da9383afb28e0f028906e1d15
aviharos <vihand@protonmail.ch>
2022-08-21 19:06:54 +0200

    refactor: add LoopHandler

---
+++ app/LoopHandler.py
@@ -0,0 +1,80 @@
+﻿# -*- coding: utf-8 -*-
+# Standard Library imports
+import json
+import os
+
+# PyPI packages
+import sqlalchemy
+from sqlalchemy import create_engine
+import psycopg2
+
+# Custom imports, config
+from conf import conf
+from Logger import getLogger
+from object_to_template import object_to_template
+from OEE import OEECalculator
+import Orion
+
+class LoopHandler():
+    logger = getLogger(__name__)
+
+    def __init__(self):
+        self.ids = {'ws': None,
+                    'job': None,
+                    'oee': None,
+                    'throughput': None}
+
+    def handle(self):
+        try:
+            self.engine = create_engine(f'postgresql://{conf["postgresUser"]}:{conf["postgresPassword"]}@{conf["postgresHost"]}:{conf["postgresPort"]}')
+            self.con = self.engine.connect()
+            self.workstations = Orion.getWorkstations()
+            if len(self.workstations) == 0:
+                self.logger.critical(f'No Workstation is found in the Orion broker, no OEE data')
+            for ws in self.workstations:
+                self.ids['ws'] = ws['id']
+                self.ids['job'] = ws['RefJob']['value']
+                if not Orion.exists(self.ids['job']):
+                    raise ValueError('Critical: object does not exist in Orion: {self.ids["job"]}')
+                self.ids['oee'] = ws['RefOEE']['value']
+                self.ids['throughput'] = ws['RefThroughput']['value']
+                oeeCalculator = OEECalculator(self.ids['ws'])
+                oeeCalculator.prepare(self.con)
+                oee = oeeCalculator.calculate_OEE()
+                throughput = oeeCalculator.calculate_throughput()
+                Orion.update((oee, throughput))
+
+        except (AttributeError,
+                KeyError,
+                RuntimeError,
+                ValueError,
+                ZeroDivisionError,
+                psycopg2.OperationalError,
+                sqlalchemy.exc.OperationalError) as error:
+            self.logger.error(error)
+            # could not calculate OEE or Throughput
+            # try to delete the OEE and Throughput values, if we have enough data
+            if not None in self.ids.values():
+                try:
+                    oee = object_to_template(os.path.join('..', 'json', 'OEE.json'))
+                    throughput = object_to_template(os.path.join('..', 'json', 'Throughput.json'))
+                except FileNotFoundError as error:
+                    self.logger.critical(f'OEE.json or Throughput.json not found.\n{error}')
+                except json.decoder.JSONDecodeError as error:
+                    self.logger.critical(f'OEE.json or Throughput.json is invalid.\n{error}')
+                else:
+                    oee['id'] = self.ids['oee']
+                    throughput['id'] = self.ids['throughput']
+                    for object_ in (oee, throughput):
+                        object_['RefWorkstation']['value'] = self.ids['ws']
+                        object_['RefJob']['value'] = self.ids['job']
+                    Orion.update((oee, throughput))
+            else:
+                self.logger.critical(f'A critical error occured, not even the references of the objects could be determined. No OEE data. An OEE and Throughput object should be cleared, but it cannot be determined, which ones.')
+        finally:
+            try:
+                self.con.close()
+                self.engine.dispose()
+            except NameError:
+                pass
+
--- app/main.py
+++ app/main.py
@@ -1,91 +1,24 @@
 ﻿# -*- coding: utf-8 -*-
-"""
-Install dependencies in Miniconda environment (just once)
-conda install python=3.8 spyder sqlalchemy pandas psycopg2
-"""
-
 # Standard Library imports
-import json
-import os
 import sched
 import time
-
-# PyPI packages
-import sqlalchemy
-from sqlalchemy import create_engine
-import psycopg2
 
 # Custom imports, config
 from conf import conf
 from Logger import getLogger
-from object_to_template import object_to_template
-from OEE import OEECalculator
-import Orion
-
+from LoopHandler import LoopHandler
 logger_main = getLogger(__name__)
 
-def handle_ws(con, ws):
-    ws_id = ws['id']
-    ref_job_id = ws['RefJob']['value']
-    if not Orion.exists(ref_job_id):
-        raise ValueError('Critical: object does not exist in Orion: {id}')
-    ref_OEE_id = ws['RefOEE']['value']
-    ref_Throughput_id = ws['RefThroughput']['value']
-    oeeCalculator = OEECalculator(ws_id)
-    oeeCalculator.prepare(con)
-    oee = oeeCalculator.calculate_OEE()
-    throughput = oeeCalculator.calculate_throughput()
-    Orion.update((oee, throughput))
-    return ws_id, ref_job_id, ref_OEE_id, ref_Throughput_id
-
-def clear_KPI_objects(ws_id, ref_job_id, ref_OEE_id, ref_Throughput_id):
-    try:
-        oee = object_to_template(os.path.join('..', 'json', 'OEE.json'))
-        throughput = object_to_template(os.path.join('..', 'json', 'Throughput.json'))
-    except FileNotFoundError as error:
-        logger_main.critical(f'OEE.json or Throughput.json not found.\n{error}')
-    except json.decoder.JSONDecodeError as error:
-        logger_main.critical(f'OEE.json or Throughput.json is invalid.\n{error}')
-    else:
-        oee['id'] = ref_OEE_id
-        throughput['id'] = ref_Throughput_id
-        for object_ in (oee, throughput):
-            object_['RefWorkstation']['value'] = ws_id
-            object_['RefJob']['value'] = ref_job_id
-        Orion.update((oee, throughput))
 
 def loop(scheduler_):
+    logger_main.info('Calculating OEE values')
+    loopHandler = LoopHandler()
     try:
-        engine = create_engine(f'postgresql://{conf["postgresUser"]}:{conf["postgresPassword"]}@{conf["postgresHost"]}:{conf["postgresPort"]}')
-        con = engine.connect()
-        workstations = Orion.getWorkstations()
-        if len(workstations) == 0:
-            logger_main.critical(f'No Workstation is found in the Orion broker, no OEE data')
-        for ws in workstations:
-            ws_id, ref_job_id, ref_OEE_id, ref_Throughput_id = handle_ws(con, ws)
-    except (AttributeError,
-            KeyError,
-            RuntimeError,
-            ValueError,
-            ZeroDivisionError,
-            psycopg2.OperationalError,
-            sqlalchemy.exc.OperationalError) as error:
+        loopHandler.handle()
+    except Exception as error:
+        # Catch any uncategorised error and log it 
         logger_main.error(error)
-        # could not calculate OEE or Throughput
-        # try to delete the OEE and Throughput values, if we have enough data
-        if ('ws_id' in locals() and
-            'ref_job_id' in locals() and
-            'ref_OEE_id' in locals() and
-            'ref_Throughput_id' in locals()):
-            clear_KPI_objects(ws_id, ref_job_id, ref_OEE_id, ref_Throughput_id)
-        else:
-            logger_main.critical(f'A critical error occured, not even the references of the objects could be determined. No OEE data. An OEE and Throughput object should be cleared, but it cannot be determined, which ones.')
     finally:
-        try:
-            con.close()
-            engine.dispose()
-        except NameError:
-            pass
         scheduler_.enter(conf['period_time'], 1, loop, (scheduler_,))
 
 
