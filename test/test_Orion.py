52f1546755c57ee318a755bc433e50202059a1ff
aviharos <vihand@protonmail.ch>
2022-08-22 18:34:25 +0200

    test: Orion tests run OK

--- test/test_Orion.py
+++ test/test_Orion.py
@@ -10,15 +10,25 @@ from unittest.mock import patch, Mock
 
 # Custom imports
 sys.path.insert(0, os.path.join('..', 'app'))
-from modules.remove_orion_metadata import remove_orion_metadata
 import Orion
+from modules.remove_orion_metadata import remove_orion_metadata
 
-orion_entities = 'http://localhost:1026/v2/entities'
+ORION_HOST = os.environ.get('ORION_HOST')
+ORION_PORT = os.environ.get('ORION_PORT')
+
+orion_entities = f'http://{ORION_HOST}:{ORION_PORT}/v2/entities'
 
 class testOrion(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
-        pass
+        with open(os.path.join('..', 'json', 'Core001.json'), 'r') as f:
+            cls.obj = json.load(f)
+        requests.post(url=orion_entities, json=cls.obj)
+        with open(os.path.join('..', 'json', 'Workstation.json'), 'r') as f:
+            cls.ws1 = json.load(f)
+        cls.ws2 = cls.ws1.copy()
+        cls.ws2['id'] = "urn:ngsi_ld:Workstation:2"
+        cls.ws2['RefJob']['value'] = "urn:ngsi_ld:Job:2000000"
 
     @classmethod
     def tearDownClass(cls):
@@ -31,17 +41,60 @@ class testOrion(unittest.TestCase):
         pass
 
     def test_getRequest(self):
-        with open(os.path.join('..', 'json', 'Core001.json'), 'r') as f:
-            json_ = json.load(f)
-        requests.post(url=orion_entities, json=json_)
-        status_code, downloaded_json = Orion.getRequest(f'{orion_entities}/{json_["id"]}')
+        status_code, downloaded_json = Orion.getRequest(f'{orion_entities}/{self.obj["id"]}')
         downloaded_json = remove_orion_metadata(downloaded_json)
         self.assertEqual(status_code, 200)
-        self.assertEqual(downloaded_json, json_)
+        self.assertEqual(downloaded_json, self.obj)
         with patch('requests.get') as mocked_get:
             mocked_get.side_effect = ValueError
             with self.assertRaises(RuntimeError):
-                Orion.getRequest(f'{orion_entities}/{json_["id"]}')
+                Orion.getRequest(f'{orion_entities}/{self.obj["id"]}')
+
+    def test_get(self):
+        self.assertEqual(remove_orion_metadata(Orion.get('urn:ngsi_ld:Part:Core001')), self.obj)
+        with patch('requests.get') as mocked_get:
+            mocked_get.status_code = 201
+            with self.assertRaises(RuntimeError):
+                Orion.get('urn:ngsi_ld:Part:Core001')
+
+    def test_exists(self):
+        self.assertTrue(Orion.exists('urn:ngsi_ld:Part:Core001'))
+        self.assertFalse(Orion.exists('urn:ngsi_ld:Part:Core123'))
+
+    def test_getWorkstations(self):
+        requests.delete(url=f'{orion_entities}/{self.ws1["id"]}')
+        requests.delete(url=f'{orion_entities}/{self.ws2["id"]}')
+        self.assertEqual(len(Orion.getWorkstations()), 0)
+        # print(ws1)
+        # print(ws2)
+        requests.post(url=orion_entities, json=self.ws1)
+        requests.post(url=orion_entities, json=self.ws2)
+        downloaded_workstations = [remove_orion_metadata(ws) for ws in Orion.getWorkstations()]
+        self.assertEqual(len(downloaded_workstations), 2)
+        if downloaded_workstations[0]['id'] == self.ws1['id']:
+            self.assertEqual(downloaded_workstations[0], self.ws1)
+            self.assertEqual(downloaded_workstations[1], self.ws2)
+        else:
+            self.assertEqual(downloaded_workstations[0], self.ws2)
+            self.assertEqual(downloaded_workstations[1], self.ws1)
+
+    def test_update(self):
+        ws1m = self.ws1.copy()
+        ws1m['RefJob']['value'] = 'urn:ngsi_ld:Job:12'
+        ws2m = self.ws2.copy()
+        ws2m['RefOEE']['value'] = 'urn:ngsi_ld:OEE:3'
+        requests.post(url=orion_entities, json=ws1m)
+        requests.post(url=orion_entities, json=ws2m)
+        Orion.update((ws1m, ws2m))
+        downloaded_workstations = [remove_orion_metadata(ws) for ws in Orion.getWorkstations()]
+        self.assertEqual(len(downloaded_workstations), 2)
+        if downloaded_workstations[0]['id'] == ws1m['id']:
+            self.assertEqual(downloaded_workstations[0], ws1m)
+            self.assertEqual(downloaded_workstations[1], ws2m)
+        else:
+            self.assertEqual(downloaded_workstations[0], ws2m)
+            self.assertEqual(downloaded_workstations[1], ws1m)
+
 
 def main():
     ans = input('''The testing process needs MOMAMS up and running on localhost.
